bc89fdd450becf86dbba0e6d72918161937ba9a1
qedk <1994constant@gmail.com>
2021-10-05 02:46:55 +0530

    Fix deploy script

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -5,7 +5,7 @@
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  await deploy("MoonSwapV1", {
+  await deploy("MoonSwap", {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
     //args: [ "Hello", ethers.utils.parseEther("1.5") ],
