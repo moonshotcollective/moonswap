a1595b56ed44125d430f3acb8973e100962b3e9e
qedk <1994constant@gmail.com>
2021-10-08 19:37:01 +0530

    Add custom deployment scripts and hardhat-etherscan

---
+++ packages/hardhat/scripts/deploycustom.js
@@ -0,0 +1,16 @@
+const hre = require('hardhat')
+
+async function main () {
+  const MoonSwap = await hre.ethers.getContractFactory('MoonSwap')
+  const moonSwap = await MoonSwap.deploy()
+  console.log(moonSwap)
+  await moonSwap.deployed()
+  console.log('MoonSwap deployed to:', moonSwap.address)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
