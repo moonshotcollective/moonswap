c9e5a92a6583f76412e8352b2235e3e0402b1254
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-11-29 00:42:24 +0530

    fix home page

--- packages/react-app/src/components/SwapItem.jsx
+++ packages/react-app/src/components/SwapItem.jsx
@@ -2,11 +2,16 @@ import React, { useEffect, useState } from "react";
 import { useThemeSwitcher } from "react-css-theme-switcher";
 import { Typography, Button } from "antd";
 import { NETWORK } from "../constants";
+import { getTokenData } from "../helpers/utils";
+import { Address } from ".";
 
-export default function SwapItem({ onClick, hash, index, localProvider, chainId, ...props }) {
+export default function SwapItem({ onClick, hash, index, localProvider, chainId, swapData, ...props }) {
   const { currentTheme } = useThemeSwitcher();
   const [loading, updateLoading] = useState(true);
   const [txData, updateTxData] = useState({});
+
+  const [tokenInData, setTokenInData] = useState({});
+  const [tokenOutData, setTokenOutData] = useState({});
 
   const checkTx = async () => {
     const _tx = await localProvider.waitForTransaction(hash, 1);
@@ -21,7 +26,17 @@ export default function SwapItem({ onClick, hash, index, localProvider, chainId,
     checkTx();
   }, []);
 
+  useEffect(() => {
+    console.log("SWAPDATA in SWAPITEM", { swapData });
+    if (swapData) {
+      getTokenData(swapData.inToken, localProvider).then(data => setTokenInData(data));
+      getTokenData(swapData.outToken, localProvider).then(data => setTokenOutData(data));
+    }
+  }, [swapData]);
+
   const explorer = NETWORK(chainId).blockExplorer || `https://etherscan.io/`;
+
+  if (!swapData) return null;
 
   return (
     <div style={{ width: "100%" }}>
@@ -38,14 +53,26 @@ export default function SwapItem({ onClick, hash, index, localProvider, chainId,
       >
         <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", width: "100%" }}>
           <Typography>{index}</Typography>
-          <Typography.Text copyable={{ text: hash }}>
+          <Address address={swapData.inTokenParty} fontSize={16} />
+          <Typography.Text copyable={{ text: `${explorer}address/${swapData.inToken}` }}>
             <a
               style={{ color: currentTheme === "light" ? "#222222" : "#ddd" }}
               target="_blank"
-              href={`${explorer}tx/${hash}`}
+              href={`${explorer}address/${swapData.inToken}`}
               rel="noopener noreferrer"
             >
-              {hash}
+              {swapData.tokensIn.toNumber()} {tokenInData ? tokenInData.symbol : swapData.inToken}
+            </a>
+          </Typography.Text>
+          <Address address={swapData.outTokenParty} fontSize={16} />
+          <Typography.Text copyable={{ text: `${explorer}address/${swapData.outToken}` }}>
+            <a
+              style={{ color: currentTheme === "light" ? "#222222" : "#ddd" }}
+              target="_blank"
+              href={`${explorer}address/${swapData.outToken}`}
+              rel="noopener noreferrer"
+            >
+              {swapData.tokensOut.toNumber()} {tokenOutData ? tokenOutData.symbol : swapData.outToken}
             </a>
           </Typography.Text>
           <Button onClick={onClick}>Commit Swap</Button>
--- packages/react-app/src/views/SwapList.jsx
+++ packages/react-app/src/views/SwapList.jsx
@@ -6,23 +6,12 @@ import { Address, Balance, SwapItem, AddressInput } from "../components";
 import { useParams, useHistory } from "react-router-dom";
 import { Router } from "@uniswap/sdk";
 
-export default function SwapList({
-  purpose,
-  setPurposeEvents,
-  address,
-  mainnetProvider,
-  localProvider,
-  yourLocalBalance,
-  price,
-  tx,
-  readContracts,
-  writeContracts,
-  chainId,
-}) {
+export default function SwapList({ localProvider, readContracts, chainId }) {
   const history = useHistory();
 
   const [activeSwaps, setActiveSwaps] = useState([]);
   const [swapIds, setSwapIds] = useState([]);
+  const [swapData, setSwapData] = useState([]);
 
   const getActiveSwaps = async () => {
     let swaps = null;
@@ -30,80 +19,100 @@ export default function SwapList({
     let tempIds = [];
     if (readContracts?.MoonSwap) {
       swaps = await readContracts.MoonSwap.getActiveSwaps();
-      console.log("swaps", swaps);
       swaps.forEach(swap => {
         tempIds.push(swap.toNumber());
       });
-      console.log("viewswap ", swaps);
-      console.log("tempIds ", tempIds);
-      console.log("before sorting tempIds", tempIds);
 
       tempIds = tempIds.sort((a, b) => b - a);
       tempIds.map((id, index) => {
         temp.push(utils.keccak256(swaps.find(swap => swap.toNumber() === id)));
       });
-      console.log("temp ", temp);
 
       setActiveSwaps(temp);
       setSwapIds(tempIds);
+
+      let output = [];
+
+      for (let id of tempIds) {
+        const data = await readContracts.MoonSwap.swaps(id);
+        output[id] = data;
+      }
+
+      setSwapData(output);
     }
   };
 
   useEffect(() => {
     console.log("active Swaps", activeSwaps);
-  }, []);
+  }, [activeSwaps]);
 
   useEffect(() => {
-    console.log("readContract", readContracts);
-  }, []);
+    console.log({ swapData });
+  }, [swapData]);
 
   useEffect(async () => {
     getActiveSwaps();
-  }, [activeSwaps]);
+  }, []);
 
   return (
     <div>
       <div style={{ marginBottom: 25, flex: 1 }}>
         <Card title="List of Swaps" style={{ width: "100%" }}>
-          <List
-            header={
-              <Row>
-                <Col span={6} style={{ fontSize: "20px", textDecoration: "underline" }}>
-                  Swap Id
-                </Col>
-                <Col span={12} style={{ fontSize: "20px", textDecoration: "underline" }}>
-                  Swap Transaction Address
-                </Col>
-                <Col span={6} style={{ fontSize: "20px", textDecoration: "underline" }}>
-                  Commit To Swap
-                </Col>
-              </Row>
-            }
-            bordered
-            dataSource={activeSwaps}
-            pagination={{ pageSize: 10 }}
-            renderItem={(item, index) => (
-              <List.Item>
-                <div
-                  style={{
-                    width: "80%",
-                    margin: "auto",
-                  }}
-                >
-                  <SwapItem
-                    index={swapIds[activeSwaps.indexOf(item)]}
-                    onClick={() => {
-                      history.push(`/swap/${swapIds[activeSwaps.indexOf(item)]}`);
-                    }}
-                    hash={item}
-                    localProvider={localProvider}
-                    chainId={chainId}
-                    fontSize={14}
-                  />
-                </div>
-              </List.Item>
-            )}
-          />
+          {swapData.length > 0 ? (
+            <List
+              header={
+                <Row>
+                  <Col span={4} style={{ fontSize: "20px", textDecoration: "underline" }}>
+                    Swap Id
+                  </Col>
+                  <Col span={4} style={{ fontSize: "20px", textDecoration: "underline" }}>
+                    In party
+                  </Col>
+                  <Col span={4} style={{ fontSize: "20px", textDecoration: "underline" }}>
+                    In token
+                  </Col>
+                  <Col span={4} style={{ fontSize: "20px", textDecoration: "underline" }}>
+                    Out party
+                  </Col>
+                  <Col span={4} style={{ fontSize: "20px", textDecoration: "underline" }}>
+                    Out token
+                  </Col>
+                  <Col span={4} style={{ fontSize: "20px", textDecoration: "underline" }}>
+                    Commit To Swap
+                  </Col>
+                </Row>
+              }
+              bordered
+              dataSource={activeSwaps}
+              pagination={{ pageSize: 10 }}
+              renderItem={(item, index) => {
+                return (
+                  <List.Item>
+                    <div
+                      style={{
+                        width: "80%",
+                        margin: "auto",
+                      }}
+                    >
+                      <SwapItem
+                        index={swapIds[activeSwaps.indexOf(item)]}
+                        onClick={() => {
+                          history.push(`/swap/${swapIds[activeSwaps.indexOf(item)]}`);
+                        }}
+                        hash={item}
+                        swapData={swapData[swapIds[activeSwaps.indexOf(item)]]}
+                        localProvider={localProvider}
+                        chainId={chainId}
+                        fontSize={14}
+                      />
+                    </div>
+                  </List.Item>
+                );
+              }}
+            />
+          ) : (
+            "Loading..."
+          )}
         </Card>
       </div>
     </div>
